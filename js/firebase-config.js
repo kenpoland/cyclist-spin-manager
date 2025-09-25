9458fa48ff4f7efeb287e7053649a5de2b700975
Ken  Poland <ken.poland@gmail.com>
2025-09-25 09:59:16 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,11 +6,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const firebaseConfig = {
         apiKey: "AIzaSyCs-FWwuC4i3qVsGjlRCz0jzXW6TmYXEs4",
         authDomain: "cyclistspinmanager.firebaseapp.com",
-        databaseURL: "https://cyclistspinmanager-default-rtdb.europe-west1.firebasedatabase.app",
         projectId: "cyclistspinmanager",
         storageBucket: "cyclistspinmanager.appspot.com",
         messagingSenderId: "297071510855",
   appId: "1:297071510855:web:13143b8d3563e0aa53b862"
+          databaseURL: "https://cyclistspinmanager-default-rtdb.europe-west1.firebasedatabase.app",
     };
 
     // Initialize Firebase
